c40d90653cdc76638c745de8008e33124f748d31
vshakha10 <sq.vishu@gmail.com>
2024-10-05 09:12:19 +0530

    change

---
+++ TAILWIND CSS/Lecture-2/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container:{
+      center:true,
+      padding:'2rem'
+    },
+    screens:{
+      xs:'320px',
+      sm:'576px',
+      md:'768px',
+      lg:'1200px',
+      xl:'1400px',
+      xxl:'1500px'
+    },
+      extend: {},
+  },
+  plugins: [],
+  // corePlugins:{
+  //   preflight:false,
+  // }
+}
+
