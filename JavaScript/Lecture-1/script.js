4e6ea69a4616f3282e8d0e34c7212424bfa3262a
vshakha10 <sq.vishu@gmail.com>
2024-10-19 18:59:22 +0530

    19-10-24

---
+++ JavaScript/Lecture-1/script.js
@@ -0,0 +1,25 @@
+//output formate
+
+document.write('hello world')
+
+
+
+console.log('Hello External Javascript!!');
+
+//variables
+
+var a=10;
+var $b=20;
+// var 7c=30;
+var _d=500;
+var camelCase="this is camelCase";
+var PascalCase="this is PascalCase";
+//var @v='v';
+
+
+//console.log(7a);
+
+console.log($b);
+console.log(_d);
+console.log(camelCase);
+console.log(PascalCase);
