426510e5d1e3b0a6ed52f841cdce791000d82061
vshakha10 <sq.vishu@gmail.com>
2024-10-21 09:57:53 +0530

    21-10-24

---
+++ 5.JAVASCRIPT/Lecture-2/script.js
@@ -0,0 +1,38 @@
+// javascript variables
+// var keyword can be redeclared and it is global scope
+
+a=10;
+// console.log(a);
+
+// var a ;
+// var a = 20;   
+
+// var keyword hoisting
+
+var x ,y ,z ;
+// x = "janvi";
+// y = "vishakha";
+
+// console.log(x);
+// console.log(y)
+
+// var globalscope = "Javascript";
+// console.log(globalscope);
+
+// {
+//     var blockVariable = "j Query";
+//     console.log(blockVariable);
+// }
+
+//let keyword
+
+// let temp1 = 10;
+// console.log(temp1);
+// temp1 = 20;
+
+// let hoisting
+
+let temp1,temp2,temp3;
+temp1 = 10;
+temp2 = 20;
+
