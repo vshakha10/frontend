a97e87606adc26c8e814fd3c66672a714f0c4fef
vshakha10 <sq.vishu@gmail.com>
2024-10-22 09:22:59 +0530

    22-1024

---
+++ 5.JAVASCRIPT/Lecture-3/script.js
@@ -0,0 +1,55 @@
+/* Javascript Datatypes*/
+
+/* Primitive Datatypes / Immutable*/
+
+/*
+    String
+    Number
+    Boolean
+    Undefined
+    Null
+    Symbol
+    Bigint
+    Object
+*/
+
+/* Non-Primitive Datatypes / Mutable */
+
+/*
+    Array
+    Object
+*/
+
+/* Javascript String Method */
+/* Javascript Template Method */
+
+{
+    // let string1 = 'This is string in Javascript'
+    // let string2 = "This is string in Javascript"
+    // let string3 = `This is string in Javascript`
+    // // let string4 = 'This is 'string' in Javascript'
+    // let string4 = 'This is \'string\'  in Javascript'
+    // // let string5 = "This is "string" in Javascript"
+    // let string5 = "This is \"string\" in Javascript"
+    // let string6 = `This is \`string\` in Javascript`
+    let string7 = `This is 
+    string 
+    in Javascript`
+    let names = "world"
+
+    let string8 = `hello ${names}`
+    console.log(string8);
+    
+
+
+    // console.log(string1);
+    // console.log(string2);
+    // console.log(string3);
+    // console.log(string4);
+    // console.log(string5);
+    // console.log(string6);
+    console.log(string7);
+    
+}
+
+
