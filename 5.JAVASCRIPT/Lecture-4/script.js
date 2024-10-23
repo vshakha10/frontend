0c8501a5a6e8b9e761df7ef1a7b68eabe9b6caa7
vshakha10 <sq.vishu@gmail.com>
2024-10-23 09:54:40 +0530

    23-10-24

---
+++ 5.JAVASCRIPT/Lecture-4/script.js
@@ -0,0 +1,127 @@
+/* Javascript Operator */
+
+/*
+
+1.Arithmetic Operator
+2.Assignment Operator
+3.Logical Operator
+4.Comparision Operator
+5.Bitwise Operator
+6.Ternory Operator
+7.Unary Plus / Minus
+8.typeof
+
+*/
+
+
+/* Arithmetic Operator */
+
+// {
+//     var x = 5
+//     var y = 3
+
+//     console.log(x + y);
+//     console.log(x - y);
+//     console.log(x * y);
+//     console.log(x / y);
+//     console.log(x % y);
+//     console.log(y % x);
+//     console.log(x ** y);
+//     console.log(y ** x);
+    
+// }
+
+/* assigement Operator */
+
+// {
+//     let x = 5
+//     let y = 4
+
+//     console.log(x);
+//     console.log(y);
+//     console.log(x+=y);// x = x + y
+//     console.log(x-=y);// x = x - y
+//     console.log(x*=y);// x = x * y
+//     console.log(x/=y);// x = x / y
+//     console.log(x%=y);// x = x % y
+//     console.log(x**=y);// x = x ** y   
+// }
+
+/*comparision operator */
+// {
+//     let num = 20;
+//     let num2 = 20;
+
+//  console.log(num == num2);
+//  console.log(num != num2);
+//  console.log(num === num2);
+//  console.log(num !== num2);
+//  console.log(num < num2);
+//  console.log(num <= num2);
+//  console.log(num > num2);
+//  console.log(num >= num2);
+// }
+
+{
+    let x = false
+    let y = true
+
+    if(x && y){
+        console.log('Logical Operator');
+    }else{
+        console.log('Logical End Operator');
+    }
+}
+
+{
+    let x = false
+    let y = true
+
+    if(x || y){
+        console.log('Logical OR Operator');
+    }else{
+        console.log('Logical End Operator');
+    }
+}
+
+
+{
+    let x = false
+    let y = true
+
+     // if(!x){
+    //     console.log('Logical Operator');
+    // }else{
+    //     console.log('Logical End Operator');
+    // }
+
+    // if(!y){
+    //     console.log('Logical Operator');
+    // }else{
+    //     console.log('Logical End Operator');
+    // }
+
+    // if(!(x && y)){
+    //     console.log('Logical Operator');
+    // }else{
+    //     console.log('Logical End Operator');
+    // }
+
+    // if(!(x || y)){
+    //     console.log('Logical Operator');
+    // }else{
+    //     console.log('Logical End Operator');
+    // }
+
+    // if(!(!y)){
+    //     console.log('Logical Operator');
+    // }else{
+    //     console.log('Logical End Operator');
+    // }
+
+    // if(!(!(x && y || x))){
+    //     console.log('Logical Operator');
+    // }else{
+    //     console.log('Logical End Operator');
+    // }
+}
