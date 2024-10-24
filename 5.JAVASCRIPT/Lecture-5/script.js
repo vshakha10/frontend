cca596c486ee2c1cd7c843ab602f5f9ab30c742e
vshakha10 <sq.vishu@gmail.com>
2024-10-24 09:51:56 +0530

    24-10-24

---
+++ 5.JAVASCRIPT/Lecture-5/script.js
@@ -0,0 +1,146 @@
+/* bitwise operator */
+
+{
+    // let x = 0
+    // let y = 1
+
+    // console.log(x & y);
+    // console.log(x || y);
+    // console.log(x ^ y);
+
+
+    // let x = 0
+    // let y = 0
+
+    // console.log(x & y);
+    // console.log(x || y);
+    // console.log(x ^ y);
+    
+    
+    // let x = 1
+    // let y = 1
+
+    // console.log(x & y);
+    // console.log(x || y);
+    // console.log(x ^ y);
+}
+
+/* unary plus / minus */
+
+{
+//     let x = 1
+//     let y = 3
+
+//     console.log(++x);
+//     console.log(--y);
+//     console.log(-(x));
+//     console.log(+(y));
+//     console.log(x++);
+//     console.log(x);
+//     console.log(y--);
+//     console.log(y);
+// 
+}
+
+
+ /* Ternary Operator   ? : */
+
+ {
+    // let x = true
+    // let y = false
+
+    // console.log(x || y ? "Hello" : "World");
+ }
+
+
+
+ /* Javascript Statements */
+
+/*
+    if statement
+    if.. else.. statement
+    if.. else.. if.. statement
+    switch statement
+    nested if.. else.. statement
+*/
+
+
+/* if statement */
+
+{
+    // let value = false
+
+
+    // if(value == true){
+    //     console.log('Hello Javascript!'); 
+    // }
+
+
+    // if(value == true){
+    //     console.log('Hello Javascript!'); 
+    // }else{
+    //     console.log('Hello World!'); 
+    // }
+}
+
+
+/* if.. else .. statement  */
+{
+    // let votingCard = false
+    // let age = 17
+
+    // if (votingCard == true) {
+    //     if (age >= 18) {
+    //         console.log('You Are Eligible For Voting!');
+    //     }else{
+    //         console.log('You Are Not Eligible For Voting!');
+    //     }
+    // } else {
+    //     console.log('You Are Not Eligible For Voting!');
+    // }
+}
+
+/*  if.. else.. if.. statement /  nested if.. else.. statement */
+
+{
+    // let book = 'SocialScience'
+
+    // if(book === 'Chemistry'){
+    //     console.log('Chemistry Book');
+    // }else if(book === 'Physics'){
+    //     console.log('Physics Book');
+    // }else if(book === 'English'){
+    //     console.log('English Book');
+    // }else if(book === 'Biology'){
+    //     console.log('Biology Book');
+    // }else if(book === 'Drawing'){
+    //     console.log('Drawing Book');
+    // }else if(book === 'Maths'){
+    //     console.log('Maths Book');
+    // }else{
+    //     console.log('Book Not Found');
+    // }
+}
+
+/*  switch statement */
+
+{
+    let book = 'Sanskrit'
+
+    switch (book) {
+        case 'Maths': console.log('Maths Book');
+            break;
+        case 'Drawing': console.log('Drawing Book');
+            break;
+        case 'Biology': console.log('Biology Book');
+            break;
+        case 'English': console.log('English Book');
+            break;
+        case 'Physics': console.log('Physics Book');
+            break;
+        case 'Chemistry': console.log('Chemistry Book');
+            break;
+        default:console.log('Book Not Found')
+            break;
+    }
+}
