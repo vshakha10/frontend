886c9f7889bf892fa02bab6a432571f2a20ed80b
vshakha10 <sq.vishu@gmail.com>
2024-11-13 09:44:43 +0530

    updated commit

---
+++ 5.JAVASCRIPT/Lecture-6/script.js
@@ -0,0 +1,93 @@
+/* Java script loops */
+
+
+/*
+
+while loop
+do-while loop
+for loop
+for..in loop
+for..of loop
+Nested loop
+
+*/
+
+/* while loop */
+
+
+/* syntax */
+{
+    // while(condition){
+    //     statments,
+    //     afterthought
+    // }
+}
+
+{
+    // let num = 0
+
+    // while(num < 10){
+    //     console.log(`loop count ${num}`);
+    //     num++;
+    // }
+}
+
+/* do-while loop */
+
+/* syntax */
+
+{
+    // do{
+    //     // body of statements
+    //     // afterthought
+    // }while(condition)
+}
+
+{
+    // let mark = 5
+
+    // do{
+    //     console.log(`do-while loop ${mark}`);
+    //     mark++;
+    // }   while(mark < 8)
+}
+
+/* for loop */
+
+/* syntax */
+
+{
+    // for(initilization; condition, afterthought){
+    //     body statement
+    // }
+}
+
+{
+    // for(let i=0 ; i<20 ; i++){
+    //     console.log(`for loop - ${i}`);
+        
+    // }
+}
+
+{
+    // let i = 0
+
+    // for(i ; i < 20; i++){
+    //     console.log(`for loop - ${i}`);
+        
+    // }
+}
+
+{
+    // let x=20
+    // x=50
+
+    // console.log(x);
+
+    const x = 20
+    x = 50
+
+    console.log(x);
+    
+    
+}
---
+++ 5.JAVASCRIPT/Practice/script.js
@@ -0,0 +1,110 @@
+// 1.Add two numbers input by the user
+
+// let num1 = prompt("enter number1: ");
+// let num2 = prompt("enter number2: ");
+
+// alert(`Addition is: ${Number(num1) + Number(num2)}`);
+
+// 2.Calculate the area of a rectangle
+
+// let length = 10;
+// let width = 50;
+
+// console.log(`Area of rectangle is ${length * width}`);
+
+// let area = calculateRectangleArea(length, width);
+// console.log("The area of the rectangle is: " + area);
+
+
+// 3.Convert temperature from Celsius to Fahrenheit
+
+// let celsius = 27;
+// let fahrenheit = ((celsius * 9) / 5) + 32;
+// console.log("Fahrenheit is : ", fahrenheit);
+
+// 4.Find the remainder when one number is divided by another
+
+// console.log(10 % 6);
+
+// 5.Calculate the power of a number
+
+// let value = 5;
+// let power = 3;
+
+// console.log(`Power of a number ${value} is ${value ** power}`);
+
+// 6. Combination of Arithmetic Operators
+
+// {
+//     console.log(5 + 5);
+//     console.log(25 - 5);
+//     console.log(25 * 25);
+//     console.log(10 / 5);
+//     console.log(10 % 5);
+//     console.log(12 ** 2);
+// }
+
+// 7.Comparing Numbers [5 number]
+
+let n1 = 10;
+let n2 = 55;
+
+if (n1 > n2) {
+    console.log("n1 is greater");
+} else if (n1 < n2) {
+    console.log("n2 is greater");
+} else if (n1 == n2) {
+    console.log("n1 is equal to n2");
+} else {
+    console.log("n1 is not equal to n2");
+}
+
+// 8.Comparing Strings [5 String]
+
+// {
+//     let val1 = "javascript";
+//     let val2 = "Javascript";
+
+//     console.log(val1 == val2);
+//     console.log(val1 === val2);
+// }
+
+// 9.Comparing Different Types [4 types]
+
+// {
+//     let compareX = 10;
+//     let compareY = "10";
+//     let str = "10";
+//     let ud;
+//     let value = null;
+
+//     console.log(compareX == compareY);
+//     console.log(compareX === compareY);
+//     console.log(compareY === str);
+//     // console.log(typeof ud);
+//     console.log(ud === value);
+//     console.log("0" === false);
+// }
+
+// 10. Using Multiple Operators [4 types]
+
+// {
+//     console.log(5 + 2 - 3);
+//     console.log((10 + 20) * 10);
+
+//     let a = 15;
+//     let b = 700;
+//     let c = 92;
+    
+//     if ((a > b) && (a > c)) {
+//         console.log("a is greater than b and c");
+//     } else if ((b > a) && (b > c)) {
+//         console.log("b is greater than a and c");
+//     } else if ((c > a) && (c > b)) {
+//         console.log("c is greater than a and b");
+//     } else {
+//         console.log("Invalid");
+//     }
+
+//     console.log((8 * 6) + (79 / 3));
+// }
