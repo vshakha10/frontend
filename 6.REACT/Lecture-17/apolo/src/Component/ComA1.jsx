00bec330503f61f1e8e7d09ce17160a7571cb81f
vshakha10 <sq.vishu@gmail.com>
2025-02-24 18:06:25 +0530

    28-02-2025

---
+++ 6.REACT/Lecture-17/apolo/src/Component/ComA1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ComA1 = () => {
+  return (
+    <div>
+      <h1 className='heading'>ComA1</h1>
+    </div>
+  )
+}
+
+export default ComA1
---
+++ 6.REACT/Lecture-17/apolo/src/Component/ComB2.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ComB3 from './ComB3'
+
+const ComB2 = () => {
+  return (
+    <div>
+      <h1 className='heading'>ComB2</h1>
+      <ComB3/>
+    </div>
+  )
+}
+
+export default ComB2
